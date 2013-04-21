06e2e979c6afa014afdbf494c7e71dfce244cf3a
Rob Sawyer <robksawyer@gmail.com>
2013-04-21 00:44:14 -0700

    Small tweak

--- Config/amazon.php
+++ Config/amazon.php
@@ -1,11 +1,11 @@
 <?php
-  /**
-   * Get a key and secret from AWS and fill in this content.
-   */
-  $config = array(
-  	'Aws' => array(
-  		'key' => CC_S3_ACCESS_KEY,
-  		'secret' => CC_S3_SECRET_KEY
-    )
-  );
+/**
+* Get a key and secret from AWS and fill in this content.
+*/
+	$config = array(
+		'Aws' => array(
+			'key' => CC_S3_ACCESS_KEY,
+			'secret' => CC_S3_SECRET_KEY
+		)
+	);
 ?>
