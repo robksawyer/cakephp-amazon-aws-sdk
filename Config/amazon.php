f101476af68c4885ecc582b9c71aeef79559ecfe
Rob Sawyer <robksawyer@gmail.com>
2013-08-19 09:42:10 -0700

    Updated the config

--- Config/amazon.php
+++ Config/amazon.php
@@ -4,8 +4,11 @@
 */
 	$config = array(
 		'Aws' => array(
+			'url' => getenv('CC_S3_URL'),
 			'key' => getenv('CC_S3_ACCESS_KEY'),
 			'secret' => getenv('CC_S3_SECRET_KEY')
 		)
 	);
+	Configure::write('Aws', $config);
+	$amazon_config_data = Configure::read('Aws');
 ?>
